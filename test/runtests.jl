fa55171cd3424afbc691f133938feaf05f432e39
dforero0896 <df.forero10@gmail.com>
2022-09-18 23:19:46 +0200

    Fork so I can add the package from github

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using Recfast
 using Test
 
+
 @testset "Recfast.jl" begin
-    # Write your tests here.
+    
+    @show fieldnames(Params)
+    params = Params()
+    sol = Evaluate_recombination(params, logzstart = 4., logzend = -5,dt = 1e-2, dtmin = 1e-30)
+    
+    x_He = sol.u[1,:]
+    x_H = sol.u[2,:]
+    T_m = sol.u[3,:]
+
+
 end
